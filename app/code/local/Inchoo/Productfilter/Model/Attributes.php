3ba2f3e8c0e4a8f5923ebf1b609b1ebd4cba813d
hamidin my id <hamidin@hamidin-hidayat.my.id>
2016-08-25 10:11:47 +0000

    first commit

---
+++ app/code/local/Inchoo/Productfilter/Model/Attributes.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * All attributes option array
+ *
+ * @category   Inchoo
+ * @package    Productfilter
+ * @author     Milovan Gal <milovan.gal@inchoo.net>
+ */
+class Inchoo_Productfilter_Model_Attributes
+{
+
+    public function toOptionArray()
+    {
+    	$ignoreAttributes = array('sku', 'name', 'attribute_set_id', 'type_id', 'qty', 'price', 'status', 'visibility');
+    	
+    	$collection = Mage::getResourceModel('catalog/product_attribute_collection')
+            ->addVisibleFilter();
+        
+        $result = array();   
+    	foreach ($collection as $model) {
+    		if(in_array($model->getAttributeCode(), $ignoreAttributes)) {
+    			continue;
+    		}
+    		$productCollection = Mage::getModel('catalog/product')->getCollection();
+    		$productCollection->addAttributeToSelect(array($model->getAttributeCode()));
+    		$productCollection->addAttributeToFilter($model->getAttributeCode(), array('gt' => 0));
+    		
+    		if(count($productCollection->getData()) > 0) {
+    			$result[] = array('value' => $model->getAttributeCode(), 'label'=>$model->getFrontendLabel());
+    		}
+
+        }
+
+       return $result;
+
+    }
+}
