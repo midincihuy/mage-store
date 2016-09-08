1309645ab0eb82bdd5eb55b8efe01d8e1cef2fc5
Hamidin Hidayat <mailnidimah.hamidin@gmail.com>
2016-09-08 14:51:00 +0700

    setup script coba

---
+++ app/code/local/Inchoo/DBScript/Model/Comment.php
@@ -0,0 +1,9 @@
+<?php
+ 
+class Inchoo_DBScript_Model_Comment extends Mage_Core_Model_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init('inchoo_dbscript/comment');
+    }
+}
