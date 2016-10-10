3a7a831d88e9f7c75c97125688c3d13ca2025fd6
Hamidin Hidayat <mailnidimah.hamidin@gmail.com>
2016-10-10 15:54:53 +0700

    Revert "add module customRule"
    This reverts commit ba431775d98fc1bffd657d9c295e8d5976161e0c.

--- app/code/local/Oshop/CustomRule/Helper/Data.php
+++
@@ -1,6 +0,0 @@
-<?php
- 
-class Oshop_CustomRule_Helper_Data extends Mage_Core_Helper_Abstract
-{
- 
-}
--- app/code/local/Oshop/CustomRule/Model/Rule.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Oshop_CustomRule_Model_Rule extends Mage_SalesRule_Model_Rule{
-	const FIX_FEE = 'fix_fee';
-}
